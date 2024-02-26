4687c85a8c1f16c3dae7b7077e8397f7aaa1604a
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-26 19:45:34 +0530

    models created

--- IMS_app/models.py
+++ IMS_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Supplier(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.id} - {self.user.username}"
+    
+    
+class Product(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    selling_unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} Inventory"
