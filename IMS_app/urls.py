b2c2e807ae1315468bd2d615a5aa0b4ff477d83b
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-26 23:06:18 +0530

    AdminDashboard and supplier dashboard in progress

--- IMS_app/urls.py
+++ IMS_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.LandingView.as_view(), name='landing_page'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('admin/dashboard', views.AdminDashboardView.as_view(), name='admin_dashboard'),
+    path('supplier/dashboard', views.SupplierDashboardView.as_view(), name='supplier_dashboard'),
 ]
--- IMS_app/views.py
+++ IMS_app/views.py
@@ -1,8 +1,25 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from .mixins import AdminLoginMixin,SupplierLoginMixin
 
 class LandingView(TemplateView):
     template_name = 'landing_page.html'
     
 class CustomLoginView(LoginView):
     template_name = 'login.html'
+    
+    def get_success_url(self):
+        user = self.request.user
+        if user.is_authenticated and user.is_superuser:
+            return reverse_lazy('admin_dashboard')
+        else:
+            return reverse_lazy('supplier_dashboard')
+                        
+class AdminDashboardView(AdminLoginMixin,TemplateView):
+    template_name = 'admin_dashboard.html'
+    
+class SupplierDashboardView(SupplierLoginMixin,TemplateView):
+    template_name = 'supplier_dashboard.html'
