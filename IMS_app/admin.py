9de117ecf93906d99c79bdc2daad982b0ebdda5f
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-26 23:20:58 +0530

    inventory model added in admin site

--- IMS_app/admin.py
+++ IMS_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, Supplier
+from .models import Product, Supplier ,Inventory
 
 admin.site.register(Product)
 admin.site.register(Supplier)
+admin.site.register(Inventory)
