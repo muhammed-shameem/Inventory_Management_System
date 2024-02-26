8c0182d0f1f17533d36938450e54436e8077cef0
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-27 00:50:20 +0530

    Admin Dashboard In progress

--- IMS_app/urls.py
+++ IMS_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.LandingView.as_view(), name='landing_page'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('admin/dashboard', views.AdminDashboardView.as_view(), name='admin_dashboard'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('ims/dashboard', views.AdminDashboardView.as_view(), name='admin_dashboard'),
     path('supplier/dashboard', views.SupplierDashboardView.as_view(), name='supplier_dashboard'),
 ]
--- IMS_app/views.py
+++ IMS_app/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import reverse_lazy
+from .models import Inventory
 from .mixins import AdminLoginMixin,SupplierLoginMixin
 
 class LandingView(TemplateView):
@@ -17,9 +18,18 @@ class CustomLoginView(LoginView):
             return reverse_lazy('admin_dashboard')
         else:
             return reverse_lazy('supplier_dashboard')
+        
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
                         
 class AdminDashboardView(AdminLoginMixin,TemplateView):
     template_name = 'admin_dashboard.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        inventory_list = Inventory.objects.all()
+        context['inventory_list'] = inventory_list
+        return context
     
 class SupplierDashboardView(SupplierLoginMixin,TemplateView):
     template_name = 'supplier_dashboard.html'
