95e5c42b1753a3f12d8d472ff0ca63d832370ec2
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-26 22:00:21 +0530

    login page ui completed

--- IMS_app/urls.py
+++ IMS_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.LandingView.as_view(), name='landing_page'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
 ]
--- IMS_app/views.py
+++ IMS_app/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 class LandingView(TemplateView):
     template_name = 'landing_page.html'
+    
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
