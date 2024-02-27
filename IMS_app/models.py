eb40127f7c224bbfa43df74ebab89058eeb70234
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-27 15:46:10 +0530

    inventory list and product list completed in admin section

--- IMS_app/models.py
+++ IMS_app/models.py
@@ -16,6 +16,7 @@ class Product(models.Model):
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    active_status=models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
--- IMS_app/views.py
+++ IMS_app/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+from django.db import transaction
 from django.contrib.auth.views import LoginView,LogoutView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 from django.urls import reverse_lazy
-from .models import Inventory
+from .models import Inventory,Product
 from .mixins import AdminLoginMixin,SupplierLoginMixin
 
 class LandingView(TemplateView):
@@ -30,6 +31,46 @@ class AdminDashboardView(AdminLoginMixin,TemplateView):
         inventory_list = Inventory.objects.all()
         context['inventory_list'] = inventory_list
         return context
+    
+class AdminDashboardProductsView(AdminLoginMixin,TemplateView):
+    template_name = 'admin_product_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_list = Product.objects.all()
+        context['product_list'] = product_list
+        return context
+class ProductPurchaseView(AdminLoginMixin, View):
+    template_name = 'purchase_product.html'
+
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        return render(request, self.template_name, {'product': product})
+
+    def post(self, request, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        intake_stock=request.POST.get('stock')
+        if intake_stock:
+            intake_stock = int(intake_stock)
+            if product.stock < intake_stock:
+                return render(request, self.template_name, {'product': product, 'error': "Not enough stock available"})
+            
+            with transaction.atomic():
+                product.stock -= intake_stock
+                product.save()
+                inventory_entry = Inventory.objects.filter(product=product).first()
+                if inventory_entry:
+                    inventory_entry.stock += intake_stock
+                    inventory_entry.save()
+                else:
+                    markup_percentage = 30
+                    selling_unit_price_default = (int(product.unit_price)) * (1 + markup_percentage / 100)
+                    Inventory.objects.create(product=product, stock=intake_stock,selling_unit_price=selling_unit_price_default)
+
+            return redirect('admin_dashboard')
+
+        return render(request, self.template_name, {'product': product, 'error': "Stock missing in form"})
+
     
 class SupplierDashboardView(SupplierLoginMixin,TemplateView):
     template_name = 'supplier_dashboard.html'
