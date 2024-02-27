3bc7fe011f91c9cf7d83b32f8cfeb6be3544ca5f
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-27 14:00:34 +0530

    purchase product completed

--- IMS_app/urls.py
+++ IMS_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('ims/dashboard', views.AdminDashboardView.as_view(), name='admin_dashboard'),
+    path('ims/products', views.AdminDashboardProductsView.as_view(), name='admin_dashboard_products'),
+    path('ims/purchase/<int:product_id>', views.ProductPurchaseView.as_view(), name='purchase_product'),
     path('supplier/dashboard', views.SupplierDashboardView.as_view(), name='supplier_dashboard'),
 ]
