6c134e7b83350699faf5ebe2b061cd5173813a74
Muhammed Shameem <muhammed.shameems@simelabs.com>
2024-02-28 16:25:59 +0530

    mixin docstring updated

--- IMS_app/mixins.py
+++ IMS_app/mixins.py
@@ -3,6 +3,10 @@ from .models import Supplier
 
 
 class AdminLoginMixin:
+    """
+    Mixin for views that require an authenticated superuser (admin) to access.
+    If the user is not authenticated or is not a superuser, a PermissionDenied exception is raised.
+    """
     def dispatch(self, request, *args, **kwargs):
         user = self.request.user
         if user.is_authenticated and user.is_superuser:
@@ -12,6 +16,10 @@ class AdminLoginMixin:
         
         
 class SupplierLoginMixin:
+    """
+    Mixin for views that require an authenticated user associated with a supplier to access.
+    If the user is not authenticated or is not associated with a supplier, a PermissionDenied exception is raised.
+    """
     def dispatch(self, request, *args, **kwargs):
         user = self.request.user
         supplier = Supplier.objects.filter(user=user).first()
