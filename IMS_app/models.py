a6d2fbfbce173cc5b996017d54f08eec112875ee
muhammed-shameem <muhammadshami977@gmail.com>
2024-02-28 10:53:40 +0530

    doc string added in models

--- IMS_app/models.py
+++ IMS_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Supplier(models.Model):
+    """
+    Represents a supplier in the system.
+
+    Attributes:
+        user (User): The user associated with the supplier.
+        phone_number (str): The phone number of the supplier.
+        address (str): The address of the supplier.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
     address = models.TextField()
@@ -11,6 +19,17 @@ class Supplier(models.Model):
     
     
 class Product(models.Model):
+    """
+    Represents a product offered by a supplier.
+
+    Attributes:
+        supplier (Supplier): The supplier associated with the product.
+        name (str): The name of the product.
+        description (str): The description of the product.
+        unit_price (Decimal): The unit price of the product.
+        stock (int): The current stock quantity of the product.
+        active_status (bool): Indicates if the product is active or not.
+    """
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -24,6 +43,14 @@ class Product(models.Model):
 
 
 class Inventory(models.Model):
+    """
+    Represents the inventory of a product.
+
+    Attributes:
+        product (Product): The product associated with the inventory.
+        selling_unit_price (Decimal): The selling unit price of the product in the inventory.
+        stock (int): The current stock quantity of the product in the inventory.
+    """
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     selling_unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
